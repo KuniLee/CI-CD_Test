70e6ff4d1741e29782da133ccc8df72b46849968
KuniLee <92676972+KuniLee@users.noreply.github.com>
2022-12-14 22:15:52 +0500

    new feature
    * 1 commit
    
    * 2 commit
    
    * 3 commit

--- file.js
+++ file.js
@@ -1,4 +1,9 @@
-fetch('https://dummyjson.com/products/14')
+fetch('https://dummyjson.com/products/15')
   .then(res => res.json())
-  .then(console.log);
-//123
+  .then(console.log)
+
+function sum(a, b) {
+    return a + b
+}
+
+console.log(sum(1,5))
